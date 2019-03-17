3da7148cddb7fab6f558fe89147bec3b47560c59
Verkhoturkin <verhoturkin@gmail.com>
2019-03-17 13:21:47 +0500

    add *.jrtc to comments in task36

--- 4.JavaCollections/src/com/javarush/task/task36/task3603/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3603/Solution.java
@@ -6,7 +6,21 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 /* 
 Поиск класса по описанию
+Замени следующие слова на нужные:
+1. ClassNameToBeReplaced - имя класса, потокобезопасный аналог ArrayList, в котором все операции изменения (mutative operations) используют новую копию основного массива.
+
+2. methodNameToBeReplaced - имя метода, который в текущий список 'list' добавляет те элементы переданной коллекции, которые не содержатся в 'list'.
+
+Не оставляй закомментированный код.
+
+
+Требования:
+1. Метод main должен создавать объект потокобезопасного аналога ArrayList, соответствующего заданию.
+2. Метод main, после создания списка правильного типа, должен вызвать метод, который добавит в текущий список 'list' только те элементы переданной коллекции, которые не содержатся в 'list'.
+3. Программа должна вывести 4 строки: "A C B D".
+4. Программа не должна содержать слова "ClassNameToBeReplaced" и "methodNameToBeReplaced".
 */
+
 public class Solution {
     public static void main(String... args) {    //it's correct line
         CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
