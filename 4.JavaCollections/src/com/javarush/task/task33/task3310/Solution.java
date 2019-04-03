4ce9d478e055716e4f1ea727025f114c2d4fe118
Verkhoturkin <verhoturkin@gmail.com>
2019-04-03 17:12:04 +0500

    Done step 11 in task3310

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -247,12 +247,31 @@ Shortener (10)
         2. В классе FileStorageStrategy должны быть созданы все необходимые поля (согласно условию задачи).
         3. Методы интерфейса StorageStrategy должны быть реализованы в FileStorageStrategy таким образом, чтобы обеспечивать корректную работу Shortener созданного на его основе.
 
+Shortener (11)
+        Как ты заметил, получение идентификатора для строки требует намного больше времени, чем получение строки по идентификатору. Это ожидаемо и следует из реализации HashMap. Давай напишем четвертую стратегию OurHashBiMapStorageStrategy, которая будет лишена этого недостатка.
+        11.1. Создай класс OurHashBiMapStorageStrategy, реализующий интерфейс StorageStrategy.
+        11.2. Добавь в него два поля HashMap<Long, String> k2v и HashMap<String, Long> v2k.
+        Первое будет хранить соответствие ключа и значения, а второе наоборот: значения и ключа.
+        11.3. Реализуй методы интерфейса StorageStrategy, обеспечив максимальную скорость.
+
+        Подсказка: при добавлении новой пары ключ-значение необходимо добавлять ее сразу в два поля.
+
+        Проверь новую стратегию в методе main(). Запусти программу и сравни скорость работы всех 4х стратегий. Убедись, что мы значительно увеличили скорость получения идентификатора. Но как ты понимаешь, у этого решения есть не только плюсы, но и минусы.
+        Подумай в каких случаях имеет смысл использовать OurHashBiMapStorageStrategy, а в каких HashMapStorageStrategy.
+
+
+Требования:
+        1. Класс OurHashBiMapStorageStrategy должен поддерживать интерфейс StorageStrategy.
+        2. В классе OurHashBiMapStorageStrategy должны быть созданы и инициализированы две HashMap (k2v и v2k).
+        3. Метод containsKey должен проверять содержится ли полученный параметр в k2v.
+        4. Метод containsValue должен проверять содержится ли полученный параметр в v2k.
+        5. Метод put должен добавлять пару (key, value) в k2v и пару (value, key) в v2k.
+        6. Метод getValue должен возвращать значение полученное из k2v.
+        7. Метод getKey должен возвращать значение полученное из v2k.
+
 */
 
-import com.javarush.task.task33.task3310.strategy.FileStorageStrategy;
-import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
-import com.javarush.task.task33.task3310.strategy.OurHashMapStorageStrategy;
-import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+import com.javarush.task.task33.task3310.strategy.*;
 
 import java.util.Date;
 import java.util.Set;
@@ -305,8 +324,9 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        testStrategy(new HashMapStorageStrategy(), 1000);
-        testStrategy(new OurHashMapStorageStrategy(), 1000);
-        testStrategy(new FileStorageStrategy(), 1000);
+        testStrategy(new HashMapStorageStrategy(), 10000);
+        testStrategy(new OurHashMapStorageStrategy(), 10000);
+        testStrategy(new FileStorageStrategy(), 100);
+        testStrategy(new OurHashBiMapStorageStrategy(), 10000);
     }
 }
