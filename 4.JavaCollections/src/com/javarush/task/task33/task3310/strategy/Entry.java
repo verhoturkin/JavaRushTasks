1e166da9eac1822a1ff404538263c42b8f1a53f6
Verkhoturkin <verhoturkin@gmail.com>
2019-04-03 14:23:50 +0500

    Done step 7 in task3310

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/Entry.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task33.task3310.strategy;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Entry implements Serializable {
+    Long key;
+    String value;
+    Entry next;
+    int hash;
+
+    public Entry(int hash, Long key, String value, Entry next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+        this.hash = hash;
+    }
+
+    public Long getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return key + "=" + value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Entry entry = (Entry) o;
+
+        if (key != null ? !key.equals(entry.key) : entry.key != null) return false;
+        return value != null ? value.equals(entry.value) : entry.value == null;
+    }
+
+    public int hashCode() {
+        return Objects.hashCode(key) ^ Objects.hashCode(value);
+    }
+}
