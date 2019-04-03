6c04838df82d528e51e7285704b8680fe33bec94
Verkhoturkin <verhoturkin@gmail.com>
2019-04-03 17:39:04 +0500

    Done step 13 in task3310

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -286,6 +286,24 @@ Shortener (12)
         5. Метод put должен добавлять пару (key, value) в data.
         6. Метод getValue должен возвращать значение полученное из data.
         7. Метод getKey должен возвращать значение полученное из data.inverse().
+
+Shortener (13)
+        Рассмотрим еще одну реализацию BiMap, на этот раз из Apache Commons Collections.
+        13.1. Скачай и подключи Apache Commons Collections 4.0.
+        13.2. Реализуй стратегию DualHashBidiMapStorageStrategy. Она должна:
+        13.2.1. Поддерживать интерфейс StorageStrategy.
+        13.2.2. Внутри иметь только одно поле data с типом DualHashBidiMap.
+        13.3. Проверь новую стратегию в методе main(). Запусти программу и сравни скорость работы шести стратегий.
+
+
+Требования:
+        1. Класс DualHashBidiMapStorageStrategy должен поддерживать интерфейс StorageStrategy.
+        2. В классе DualHashBidiMapStorageStrategy должно быть создано только одно поле data типа DualHashBidiMap.
+        3. Метод containsKey() должен проверять содержится ли ключ в data.
+        4. Метод containsValue() должен проверять содержится ли значение в data.
+        5. Метод put() должен добавлять пару (key, value) в data.
+        6. Метод getValue() должен возвращать значение полученное из data.
+        7. Метод getKey() должен возвращать ключ полученный из data.getKey().
 */
 
 import com.javarush.task.task33.task3310.strategy.*;
@@ -346,5 +364,6 @@ public class Solution {
 //        testStrategy(new FileStorageStrategy(), 100);
         testStrategy(new OurHashBiMapStorageStrategy(), 10000);
         testStrategy(new HashBiMapStorageStrategy(), 10000);
+        testStrategy(new DualHashBidiMapStorageStrategy(), 10000);
     }
 }
