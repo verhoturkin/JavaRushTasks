b4bb5ee6d0a00b33db8b91f10f1f856b12d4846a
Verkhoturkin <verhoturkin@gmail.com>
2019-04-28 16:22:03 +0500

    Solved task2810 step15

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -2,14 +2,18 @@ package com.javarush.task.task28.task2810.view;
 
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class HtmlView implements View {
+    private final String filePath = "./4.JavaCollections/src/" + this.getClass().getPackage().getName().replace('.', '/') + "/vacancies.html";
     Controller controller;
-    private final String filePath = "./4.JavaCollections/src/" + this.getClass().getPackage().getName().replace('.','/') + "/vacancies.html";
 
     @Override
     public void update(List<Vacancy> vacancies) {
@@ -25,17 +29,46 @@ public class HtmlView implements View {
         controller.onCitySelect("Odessa");
     }
 
-    private String getUpdatedFileContent (List<Vacancy> vacancies){
+    private String getUpdatedFileContent(List<Vacancy> vacancies) {
+        Document doc = null;
+        try {
+            doc = getDocument();
 
-        return null;
+            Element template = doc.getElementsByClass("template").first();
+            Element copy = template.clone();
+
+            copy.removeClass("template").removeAttr("style");
+            doc.select("tr[class=vacancy]").remove().not("tr[class=vacancy template]");
+
+            for (Vacancy vacancy : vacancies) {
+                Element element = copy.clone();
+                element.getElementsByClass("city").first().text(vacancy.getCity());
+                element.getElementsByClass("companyName").first().text(vacancy.getCompanyName());
+                element.getElementsByClass("salary").first().text(vacancy.getSalary());
+                element.getElementsByTag("a").first().text(vacancy.getTitle()).attr("href", vacancy.getUrl());
+
+                template.before(element.outerHtml());
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Some exception occurred";
+        }
+
+        return doc.html();
     }
 
     private void updateFile(String content) {
-        try(FileWriter writer = new FileWriter(filePath)) {
+        try (FileWriter writer = new FileWriter(filePath)) {
             writer.write(content);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
     }
 }
