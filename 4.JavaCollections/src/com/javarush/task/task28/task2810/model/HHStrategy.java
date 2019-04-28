ba90e366acfe896fa6446c689e90024e2e738708
Verkhoturkin <verhoturkin@gmail.com>
2019-04-28 13:37:26 +0500

    Solved task2810 step6

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -15,7 +15,11 @@ public class HHStrategy implements Strategy {
     @Override
     public List<Vacancy> getVacancies(String searchString) {
         try {
-            Document document = Jsoup.connect(String.format(Locale.ENGLISH, URL_FORMAT, "Киев", 0)).get();
+            Document document = Jsoup
+                    .connect(String.format(Locale.ENGLISH, URL_FORMAT, "Киев", 0))
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.132")
+                    .referrer("no-referrer-when-downgrade")
+                    .get();
 
         } catch (IOException e) {
             e.printStackTrace();
