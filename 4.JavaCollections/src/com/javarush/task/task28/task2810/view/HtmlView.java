8f5130b034d59ab4282231fc8120f01614138e28
Verkhoturkin <verhoturkin@gmail.com>
2019-04-28 15:34:47 +0500

    Solved task2810 step12

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -6,16 +6,19 @@ import com.javarush.task.task28.task2810.vo.Vacancy;
 import java.util.List;
 
 public class HtmlView implements View {
-
     Controller controller;
 
     @Override
     public void update(List<Vacancy> vacancies) {
-
+        System.out.println(vacancies.size());
     }
 
     @Override
     public void setController(Controller controller) {
         this.controller = controller;
+    }
+
+    public void userCitySelectEmulationMethod() {
+        controller.onCitySelect("Odessa");
     }
 }
