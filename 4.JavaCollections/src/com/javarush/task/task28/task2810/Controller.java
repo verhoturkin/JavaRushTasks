6a0a99e00a715456d29ff9f495e87ac9268e5751
Verkhoturkin <verhoturkin@gmail.com>
2019-04-28 15:23:43 +0500

    Solved task2810 step11

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,36 +1,19 @@
 package com.javarush.task.task28.task2810;
 
-import com.javarush.task.task28.task2810.model.Provider;
-import com.javarush.task.task28.task2810.vo.Vacancy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.javarush.task.task28.task2810.model.Model;
 
 public class Controller {
+    private Model model;
 
-    private Provider[] providers;
-
-    public Controller(Provider... providers) {
-        if (providers == null | providers.length == 0)
+    public Controller(Model model) {
+        if(model == null)
             throw new IllegalArgumentException();
 
-        this.providers = providers;
-
+        this.model = model;
     }
 
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 
-    public void scan() {
-        List<Vacancy> vacancies = new ArrayList<>();
-        for (Provider provider : providers) {
-            vacancies.addAll(provider.getJavaVacancies("Киев"));
-        }
-        System.out.println(vacancies.size());
-    }
 }
